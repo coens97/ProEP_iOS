c24f3249f32204fd6e27779e429842fd57b0c927
Coen Stange <coen.stange@hotmail.com>
2017-11-01 21:06:38 +0100

    + add initial UI code

---
+++ Show me/Show me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Show me
+//
+//  Created by Coen Stange on 01/11/2017.
+//  Copyright © 2017 Ninja coders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
